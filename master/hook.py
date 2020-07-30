5ab046aa5b1b60fbb9280f109e616f4cc191c40c
guo <qaz813887@gmail.com>
2020-07-30 22:27:55 +0800

    add guo branch, get text id and set text

--- master/hook.py
+++ master/hook.py
@@ -7,8 +7,8 @@ app_name = "jakhar.aseem.diva"
 
 def openapp():
     os.system("adb shell pm clear "+app_name)
-    os.system("adb shell am start -n %s/%s.MainActivity"%(app_name,app_name))
-    time.sleep(1)
+    os.system("adb shell am start -n %s/%s.InsecureDataStorage4Activity"%(app_name,app_name))
+    time.sleep(5)
 
 openapp()
 os.system("adb shell input keyevent KEYCODE_BACK")
@@ -19,10 +19,12 @@ process = frida.get_usb_device().attach(app_name)
 
 script = process.create_script(jscode)
 def on_message(message, data):
-    if("app:id" in message["payload"]):
-        print(message["payload"].split("app:id/")[1][:-1])
+    print(message)
+    # if("app:id" in message["payload"]):
+    #     print(message["payload"].split("#")[1][:8])
 script.on('message', on_message)
 print('[*] Running')
 script.load()
-os.system("adb shell am start --activity-single-top %s/%s.MainActivity"%(app_name,app_name))
+os.system("adb shell am start --activity-single-top %s/%s.InsecureDataStorage4Activity"%(app_name,app_name))
 sys.stdin.read()
+
