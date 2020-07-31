221f065e03bcffa0cac1f082584d8cebecbbceb7
Yi Kuo <kuokuoyiyi@gmail.com>
2020-07-31 09:30:49 +0800

    YAY!@!

--- main/run.py
+++ main/run.py
@@ -2,6 +2,7 @@ import frida
 import sys
 import os
 import time
+from termcolor import cprint
 
 DELAY_TIME = 1
 WAIT_TIME = 2
@@ -10,16 +11,19 @@ app_name = "jakhar.aseem.diva"
 # app_name = sys.argv[1]
 
 def open_activity(activity_name):
-    print("[*] {}: Starting activity".format(activity_name))
-    os.system("adb shell pm clear {}".format(app_name))
+    print("[*] {}: Starting activity...".format(activity_name))
+    os.system("adb shell pm clear {} > /dev/null".format(app_name))
     time.sleep(DELAY_TIME)
-    os.system("adb shell am start -n {}/{}.{}".format(app_name, app_name, activity_name))
+    os.system("adb shell am start -n {}/{}.{} > /dev/null".format(app_name, app_name, activity_name))
+    print("[*] {}: Activity started".format(activity_name))
     time.sleep(DELAY_TIME)
 
 def reopen_page(activity_name):
     os.system("adb shell input keyevent KEYCODE_BACK")
     time.sleep(DELAY_TIME)
-    os.system("adb shell am start -n {}/{}.{}".format(app_name, app_name, activity_name))
+    os.system("adb shell am start -n {}/{}.{} > /dev/null".format(app_name, app_name, activity_name))
+
+
 
 def find_buttons(activity_name):
     """
@@ -27,16 +31,17 @@ def find_buttons(activity_name):
     """
 
     open_activity(activity_name)
+    
     print("[*] {}: Finding buttons".format(vuln_act))
 
-    print("[*] {}: Adding hooks".format(activity_name))
+    print("[*] {}: Adding search hooks...".format(activity_name))
     jscode = open("find_buttons.js").read()
 
     buttons = set()
     def on_message(message, data):
         if message["type"] == "error":
-            print("[!] Error in JS Code:")
-            print(message["description"])
+            cprint("[!] Error in JS Code:", "yellow")
+            cprint("[!] > " + message["description"], "yellow")
         elif "app:id" in message["payload"]:
             nonlocal buttons
             buttons.add(int(message["payload"].split("#")[1].split(" ")[0], base=16))
@@ -48,16 +53,19 @@ def find_buttons(activity_name):
     script.on('message', on_message)
     script.load()
 
-    print("[*] {}: Hooks added, reloading".format(activity_name))
+    print("[*] {}: Search hooks added, reloading activity...".format(activity_name))
     reopen_page(activity_name)
     time.sleep(WAIT_TIME)
+    print("[*] {}: Found {} buttons".format(activity_name, len(buttons)))
     return buttons
 
 
 def click_button(activity_name, button_id):
     """
-    Click button, also insert text to EditTexts
+    Click button, also insert text to EditTexts, and other security checks
     """
+    print("[*] {}: Testing button {}".format(activity_name, button_id))
+    print("[*] {}: Adding security testing hooks...".format(activity_name))
 
     jscode = open("click_button.js").read()
     jscode = jscode.replace("[BUTTONID]", str(button_id))\
@@ -66,24 +74,39 @@ def click_button(activity_name, button_id):
 
     def on_message(message, data):
         if message["type"] == "error":
-            print("[!] Error in JS Code:")
-            print(message["description"])
+            cprint("[!] Error in JS Code:", "yellow")
+            cprint("[!] > " + message["description"], "yellow")
+        else:
+            msg = message["payload"].split("#")[1]
+            if "⚠️" in msg:
+                cprint(msg.split("⚠️")[0], 'red', end="")
+                cprint("⚠️" + msg.split("⚠️")[1] + "⚠️", 'yellow', attrs=['blink'], end="")
+                cprint(msg.split("⚠️")[2], 'red')
+            elif "----------------------------------" in msg:
+                cprint(msg, 'red')
+            else:
+                cprint(msg.split(": ")[0] + ": ", 'red', end="")
+                cprint(msg.split(": ")[1], 'yellow')
 
     session = frida.get_usb_device().attach(app_name)
+
     script = session.create_script(jscode)
     script.on('message', on_message)
     script.load()
 
-    print("[*] {}: Hooks added, reloading".format(activity_name))
+    print("[*] {}: Security testing hooks added, reloading activity...".format(activity_name))
     reopen_page(activity_name)
-    time.sleep(WAIT_TIME)
+    time.sleep(WAIT_TIME*2)
 
+
+def generate_sec_js(file):
+    pass
 
 
 if __name__ == "__main__":
-    vuln_activities = ["HardcodeActivity"]
+    vuln_activities = ["LogActivity", "InsecureDataStorage4Activity", "SQLInjectionActivity"]
     for vuln_act in vuln_activities:
-        print("[*] Running test on activity: {}".format(vuln_act))
+        print("[*] {}: Running test on activity: {}".format(vuln_act, vuln_act))
 
         buttons = find_buttons(vuln_act)
         
