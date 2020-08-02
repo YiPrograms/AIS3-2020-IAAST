1b7cb55d80ae69fd69495e8648a2c90962924c56
Yi Kuo <kuokuoyiyi@gmail.com>
2020-08-02 10:17:43 +0800

    Combine codes

--- production/generate_report.py
+++ production/generate_report.py
@@ -7,15 +7,23 @@ template = open("report_template.html").read()
 
 def gen_report(app_name, issues):
     iss_lines = []
+    iss_items = {}
     for activity, details, code, cvss in issues:
         cvss_rate = cvss.split(" (")[0]
         cvss_level = cvss.split(" (")[1].split(")")[0]
+        if (activity, details, cvss_rate, cvss_level) not in iss_items:
+            iss_items[(activity, details, cvss_rate, cvss_level)] = []
+        iss_items[(activity, details, cvss_rate, cvss_level)].append(code)
+    
+    for(activity, details, cvss_rate, cvss_level), codes in iss_items.items():
+        code_lines = "\n".join("\t"*11 + "<code>" + c + "</code>" for c in codes)
         row = row_template.replace("[ACTIVITY]", activity)\
                           .replace("[DETAILS]", details)\
-                          .replace("[CODE]", code)\
+                          .replace("[CODES]", code_lines)\
                           .replace("[CVSS]", cvss_rate)\
-                          .replace("[LEVEL]", cvss_level)                            
+                          .replace("[LEVEL]", cvss_level)                         
         iss_lines.append(row)
+        
     iss = "\n".join(iss_lines)
     html = template.replace("[APPNAME]", app_name).replace("[ISSUES]", iss)
     open("IAAST_{}.html".format(app_name), "w").write(html)
